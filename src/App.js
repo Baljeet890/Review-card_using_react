773c8316c0e1d5ea0804a08a8122c333def8850d
Baljeet <0201ite015@gmail.com>
2023-01-16 16:08:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {FaGithubSquare} from 'react-icons/fa'; 
+import './compnent/Review.css';
+import Review from './compnent/Review';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+        <div className='title'>
+           <h2>our Reviews</h2>
+           <div className='underline'></div>
+        </div>
+      <Review/>
+      </section>
+    </main>
   );
 }
 
